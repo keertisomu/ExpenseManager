1b3b2a5e05e4e881a33da6fcb33b6fb177bbcdbe
Keerti Somasundaram <keerti.somasundaram@cyara.com>
2020-03-22 10:39:30 +1100

    add expense

--- app.js
+++ app.js
@@ -1,18 +1,80 @@
 console.log('expense manager');
 
-function getExpensesAsync(){
+//get list of expenses
+async function getExpensesAsync(){
     
-    fetch(`https://localhost:5001/expense`)
-    .then(result =>{
-        console.log(result);
-        return result.json();
-    })
-    .then(data => {
-        console.log(data);
-        /* const day_data = data.consolidated_weather[0];
-        //day_data = data.consolidated_weather[1];
-        console.log(`Temperature of ${data.title} for today is between ${day_data.min_temp} and ${day_data.max_temp}`)  */
-    })
-    .catch(error => console.log('some error:' + error));
-}
+    try
+    {
+        const result = await fetch(`https://localhost:5001/expense`);
+        const data = await result.json();
+        //populate html table
+        let table = document.getElementById('expenseTable');
+        data.forEach(function(object) {
+        var tr = document.createElement('tr');
+        tr.innerHTML = '<td>' + object.id + '</td>' +
+            '<td>' + object.name + '</td>' +
+            '<td>' + object.value + '</td>' +
+            '<td>' + object.category + '</td>' +
+            '<td>' + object.created + '</td>';
+        table.appendChild(tr);});
+    }
+    catch(error)
+    {
+        alert('getExpensesAsync(): ' + error);
+    }
+};
+
 getExpensesAsync();
+
+//add an expense
+
+async function addExpenseAsync(){
+    try{
+        let expName = document.getElementById('txtName').value;
+        let expVal = document.getElementById('txtValue').value;
+        let expCat = document.getElementById('txtCategory').value;
+        var today = new Date(2020 , 3 , 21);
+        /* var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '/' + dd + '/' + yyyy; */
+        let expCur = "AUD";
+        
+        let expense = {
+            id: 190,
+            created: today,
+            name: expName,
+            value: expVal,
+            category: expCat,
+            currency: expCur
+          };
+
+        let expJson = JSON.stringify(expense);
+        console.log(expJson);
+
+       const result = await fetch(`https://localhost:5001/expense`, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin' ,
+        headers: {
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+          'Content-Type': 'application/json'
+        }, 
+        body: expJson
+      });
+      
+      const content = await result.json();
+      console.log(content);
+
+    }
+    catch(error){
+        alert('addExpenseAsync(): ' + error);
+    }
+}
+
+
