359b595d1273c9285134b5e1fc6aed81c52d2437
Keerti Somasundaram <keerti.somasundaram@cyara.com>
2020-04-05 14:46:07 +1000

    add delete

--- app.js
+++ app.js
@@ -10,26 +10,37 @@ async function getExpensesAsync(){
         expenseData = await result.json();
         //populate html table
         let table = document.getElementById('expenseTable');
+
+        //clear the table
+        var rowCount = table.rows.length; 
+        if(rowCount > 0){
+            while(--rowCount) {
+                table.deleteRow(rowCount);
+            }
+        }
+
         expenseData.forEach(function(object) {
         var dtCreated = getFormattedDate(object.created);
         var tr = document.createElement('tr');
         //style="display:none;"
+        //| <button id=${object.id}>Delete</button>
         tr.innerHTML =
-            '<td >' + 'upd_' + object.id + '</td>' +
+            '<td style="display:none;">' + object.id + '</td>' +
             '<td contentEditable>' + object.name + '</td>' +
             '<td contentEditable>' + object.value + '</td>' +
             '<td contentEditable>' + object.category + '</td>' +
             '<td contentEditable>' + dtCreated + '</td>' +
-            `<td><button id=upd_${object.id}>Update</button></td>`;
+            `<td><button id=upd_${object.id}>Update</button> | <button id=del_${object.id}>Delete</button></td>`;
         table.appendChild(tr);
         document.getElementById('upd_' + object.id).addEventListener("click", updateExpenseAsync);
+        document.getElementById('del_' + object.id).addEventListener("click", delExpenseAsync);
     });
     }
     catch(error)
     {
-        alert('getExpensesAsync(): ' + error);
+        console.log('getExpensesAsync(): ' + error);
     }
-};
+}
 
 getExpensesAsync();
 
@@ -75,25 +86,26 @@ async function addExpenseAsync(){
 async function updateExpenseAsync(event){
     try{
 
-        var elementId = event.target.id
+        let elementId = event.target.id
+        //example string: upd_464664dfhf...
+        elementId = elementId.substring(4 , (elementId.length)) 
         console.log(`update : ${elementId}`);
-
         var expTable = document.getElementById('expenseTable');
-
         //gets rows of table
         var rowLength = expenseTable.rows.length;
-    
         //loops through rows
         for (i = 0; i < rowLength; i++) {
             if(i == 0){ //skip first row as its the header.
                 continue;
             }
-
             //gets cells of current row
             var cells = expenseTable.rows[i].cells;
-
             //gets amount of cells of current row
             var cellLength = cells.length;
+            //skip row if this is not the row where update was clicked.
+            if(cells[0].textContent != elementId){
+                continue;
+            }
     
            //loops through each cell in current row
            for(var j = 0; j < cellLength; j++){
@@ -137,12 +149,31 @@ async function updateExpenseAsync(event){
     catch(error){
         console.log('updateExpenseAsync(): ' + error)
     }
+}
 
     //delete expense
     async function delExpenseAsync(event){
+
+        try{
         var elementId = event.target.id
         console.log(`delete : ${elementId}`);
+        elementId = event.target.id
+        //example string: upd_464664dfhf...
+        elementId = elementId.substring(4 , elementId.length) 
+
+        const restInfo = getExpenseRestHeaders();
+        const result = await fetch(`https://localhost:5001/expense/${elementId}`, {
+                        method: 'DELETE'
+                      });
+                      const deleteStatus = result.status;
+                      console.log(deleteStatus);
+                      //reload the expense table.
+                      getExpensesAsync();
+                    }
+        catch(error){
+            alert('delExpenseAsyncerror()' + error);
+        }
     }
-}
+
 
 
